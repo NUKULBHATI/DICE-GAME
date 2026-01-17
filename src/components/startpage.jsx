075a0e7b7448290e81d39d3895fc8716b01a62e9
NUKULBHATI <nukulbhati06@gmail.com>
2026-01-17 16:06:50 +0530

    Fix image source path in startpage component

--- src/components/startpage.jsx
+++ src/components/startpage.jsx
@@ -6,7 +6,7 @@ const startpage = ({toggle}) => {
   return (
     <div className='maincontainer'>
       <div className='container'>
-        <img style={{ width: '100%', height: '100%', objectFit: 'contain' }} src="/fce5e0a76d3e531201ecd44ca61f9d27ff82e0ae.png" alt="" />
+        <img style={{ width: '100%', height: '100%', objectFit: 'contain' }} src="fce5e0a76d3e531201ecd44ca61f9d27ff82e0ae.png" alt="" />
       </div>
       <div>
         <h1 className='heading'>Dice-Game</h1>
